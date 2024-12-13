31a9c31f850f35dafba3fc10a2d9e6fe1c02de1e
Harun Dastekin <hdastekin@gmail.com>
2024-12-13 21:19:32 +0100

    working microservice ecosystem

---
+++ kommentarservice/src/main/java/com/zelkulon/kommentarservice/KommentarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zelkulon.kommentarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommentarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KommentarServiceApplication.class, args);
+	}
+
+}
---
+++ kommentarservice/src/test/java/com/zelkulon/kommentarservice/KommentarServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.zelkulon.kommentarservice;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommentarServiceApplicationTests {
+
+
+}
