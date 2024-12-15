87f8c451bf3c58e81a6affe973824dd92955d092
Harun Dastekin <hdastekin@gmail.com>
2024-12-15 20:48:14 +0100

    pom.xml

---
+++ apigateway/src/main/java/com/zelkulon/apigateway/configuration/WebConfig.java
@@ -0,0 +1,25 @@
+package com.zelkulon.apigateway.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins( "http://localhost:3000")
+                .allowedOrigins(
+                        "https://new-homepage-e1db173cf8f1.herokuapp.com"
+                )
+                .allowedOrigins("https://new-homepage-e1db173cf8f1.herokuapp.com") // Ersetze durch die URL deines Frontends
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONS hinzufügen
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
