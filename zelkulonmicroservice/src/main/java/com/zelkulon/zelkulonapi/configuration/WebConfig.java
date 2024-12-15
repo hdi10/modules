6ecd672b3f4c688d92f2219544f7f57a5134e95d
Harun Dastekin <hdastekin@gmail.com>
2024-12-15 22:36:18 +0100

    blogs funktioniert auf HEROKU!

--- zelkulonmicroservice/src/main/java/com/zelkulon/zelkulonapi/configuration/WebConfig.java
+++ zelkulonmicroservice/src/main/java/com/zelkulon/zelkulonapi/configuration/WebConfig.java
@@ -1,7 +1,5 @@
 package com.zelkulon.zelkulonapi.configuration;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,14 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedOrigins( "http://localhost:3000")
                 .allowedOrigins(
-                        "https://new-homepage-e1db173cf8f1.herokuapp.com"
+                        "http://localhost:8080",
+                        "http://localhost:3000",
+                        "https://www.zelkulon.com", // Hauptdomain
+                        "https://new-homepage-e1db173cf8f1.herokuapp.com",
+                        "https://zelkulonmicroservice-myproject-1df345e27274.herokuapp.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONS hinzufügen
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
