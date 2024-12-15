4c7f04307f5ce37239a1d7db7d171fb11427c102
Harun Dastekin <hdastekin@gmail.com>
2024-12-15 11:15:12 +0100

    :8080/blogs/blogs

--- apigateway/src/main/java/com/zelkulon/apigateway/ApiGatewayMainApplication.java
+++ apigateway/src/main/java/com/zelkulon/apigateway/ApiGatewayMainApplication.java
@@ -3,11 +3,24 @@ package com.zelkulon.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayMainApplication {
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("zelkulonmicroservice1", r -> r.path("/blogs/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://ZELKULONMICROSERVICE"))
+				.route("authservice1", r -> r.path("/auth/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://AUTH-SERVICE"))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayMainApplication.class, args);
