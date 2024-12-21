0620aef355e9ba14b548bcea7232a78d3ab5ad4f
Harun Dastekin <hdastekin@gmail.com>
2024-12-21 02:59:29 +0100

    cors header

--- apigateway/src/main/java/com/zelkulon/apigateway/configuration/WebConfig.java
+++ apigateway/src/main/java/com/zelkulon/apigateway/configuration/WebConfig.java
@@ -1,6 +1,5 @@
 package com.zelkulon.apigateway.configuration;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,16 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedOrigins( "http://localhost:3000")
+        registry.addMapping("/**") // Für alle Endpunkte gültig
                 .allowedOrigins(
-                        "https://new-homepage-e1db173cf8f1.herokuapp.com"
+                        "http://localhost:8080",
+                        "http://localhost:3000",
+                        "https://new-homepage-e1db173cf8f1.herokuapp.com", // Heroku Frontend
+                        "https://www.zelkulon.com" // Vercel Frontend
                 )
-                .allowedOrigins("https://new-homepage-e1db173cf8f1.herokuapp.com/") // Ersetze durch die URL deines Frontends
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONS hinzufügen
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Alle relevanten Methoden erlauben
+                .allowedHeaders("*") // Alle Header erlauben
+                .allowCredentials(true); // Cookies/Tokens erlauben
     }
 }
-
